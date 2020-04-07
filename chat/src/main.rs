f3fd6a75f0ac26ded12b2af519f7e9672a1314ab
Davis Van Sant <davisvansant@users.noreply.github.com>
2020-04-07 13:39:38 -0700

    initial chat receiving messages

--- chat/src/main.rs
+++ chat/src/main.rs
@@ -1,7 +1,8 @@
 use async_std::{
     prelude::*,
     task,
-    net::{TcpListener, ToSocketAddrs},
+    net::{TcpListener, ToSocketAddrs, TcpStream},
+    io::BufReader
 };
 
 type Result<T> = std::result::Result<T, Box<dyn std::error::Error + Send + Sync>>;
@@ -10,10 +11,46 @@ async fn accept_loop(addr: impl ToSocketAddrs) -> Result<()> {
     let listener = TcpListener::bind(addr).await?;
     let mut incoming = listener.incoming();
     while let Some(stream) = incoming.next().await {
-
+        let stream = stream?;
+        println!("Accepting from: {}", stream.peer_addr()?);
+        let handle = task::spawn(connection_loop(stream));
+        handle.await?
     }
 
     Ok(())
+}
+
+async fn connection_loop(stream: TcpStream) -> Result<()> {
+    let reader = BufReader::new(&stream);
+    let mut lines = reader.lines();
+
+    let name = match lines.next().await {
+        None => Err("peer disconnected immediately")?,
+        Some(line) => line?,
+    };
+    println!("name = {:?}", name);
+
+    while let Some(line) = lines.next().await {
+        let line = line?;
+        let (dest, msg) = match line.find(':') {
+            None => continue,
+            Some(idx) => (&line[..idx], line[idx + 1 ..].trim()),
+        };
+        let dist: Vec<String> = dest.split(',').map(|name| name.trim().to_string()).collect();
+        let msg: String = msg.to_string();
+    }
+    Ok(())
+}
+
+fn spawn_and_log_error<F>(fut: F) -> task::JoinHandle<()>
+where
+    F: Future<Output = Result<()>> + Send + 'static,
+{
+    task::spawn(async move {
+        if let Err(e) = fut.await {
+            eprintln!("{}", e)
+        }
+    })
 }
 
 fn run() -> Result<()> {
